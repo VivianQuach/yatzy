095ebc2628afb22bb546b7a7205982f4e25012ae
Vivian Quach <nquac016@uottawa.ca>
2024-06-18 19:18:35 -0400

    task 2, implemented dice class

---
+++ app/models/Dice.php
@@ -0,0 +1,14 @@
+<?php 
+namespace yatzy\app\models;
+
+class Dice {
+    function roll(){
+        return rand(1,6); 
+    }
+    function multi_roll($num_roll){
+        $roll_values = array(); 
+        for($x = 0; $x < $num_roll; $x++){
+            $roll_values[] = rand(6);
+        }
+    }
+}
