4b52fcbe3ee055c5401d5db6b288a47f75e55ba9
Vivian Quach <nquac016@uottawa.ca>
2024-06-28 15:39:55 -0400

    roll a die with AJAX

--- public/index.php
+++ public/index.php
@@ -1,43 +1,27 @@
 <?php
 require_once('_config.php');
-
-use Yatzy\Dice;
-
-switch ($_GET["action"] ?? "version") {
-case "roll":
-    $d = new Dice();
-    $data = ["value" => $d->roll()];
-    break;
-case "version":
-default:
-    $data = ["version" => "1.0"];
-}
-
-header("Content-Type: application/json");
-echo json_encode($data);
 ?>
 
-<div id="output">--</div>
-<button id="version">Version</button>
+<div id="die1">--</div>
+<button id="roll">Roll</button>
 
 <script>
-const output = document.getElementById("output");
-const version = document.getElementById("version");
+const die1 = document.getElementById("die1");
+const roll = document.getElementById("roll");
+roll.onclick = function(e) {
 
-version.onclick = function(e) {
+  const xmlhttp = new XMLHttpRequest();
 
-const xmlhttp = new XMLHttpRequest();
-
-xmlhttp.onreadystatechange = function() {
-  if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-    if (xmlhttp.status == 200) {
-      output.innerHTML = xmlhttp.responseText;
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+      if (xmlhttp.status == 200) {
+        die1.innerHTML = xmlhttp.responseText;
+      }
     }
-  }
-};
+  };
 
-xmlhttp.open("GET", "/api.php", true);
-xmlhttp.send();
+  xmlhttp.open("GET", "/api.php?action=roll", true);
+  xmlhttp.send();
 }
 </script>
 
