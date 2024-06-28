275407467236b3e1d81220317c4974e176089787
Vivian Quach <nquac016@uottawa.ca>
2024-06-28 15:25:52 -0400

    hook up the button with AJAX call

--- public/index.php
+++ public/index.php
@@ -4,26 +4,25 @@ require_once('_config.php');
 
 <div id="output">--</div>
 <button id="version">Version</button>
-<button id="roll-die">Roll Die</button>
 
 <script>
 const output = document.getElementById("output");
 const version = document.getElementById("version");
-const rollDie = document.getElementById("roll-die");
 
 version.onclick = function(e) {
-  output.innerHTML = "Look up version clicked";
-}
 
-rollDie.onclick = function(e) {
-  fetch('roll_die.php')
-    .then(response => response.json())
-    .then(data => {
-      output.innerHTML = "Rolled: " + data.roll;
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+const xmlhttp = new XMLHttpRequest();
+
+xmlhttp.onreadystatechange = function() {
+  if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+    if (xmlhttp.status == 200) {
+      output.innerHTML = xmlhttp.responseText;
+    }
+  }
+};
+
+xmlhttp.open("GET", "/api.php", true);
+xmlhttp.send();
 }
 </script>
 
